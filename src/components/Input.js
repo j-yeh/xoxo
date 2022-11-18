ae40a7010a625d6dd9db5cd7af8c5f13a6177bfb
Jane Yeh <jane.yeh72@gmail.com>
2022-11-18 11:50:09 -0500

    can emmit message

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,13 +1,36 @@
 import React from 'react';
+import io from 'socket.io-client';
 
-const Input = () => {
-  return (
-    <div>
-      <form>
-        <input placeholder="type here"></input>
-        <button>Send</button>
-      </form>
-    </div>
-  );
-};
+// const socket = io.connect('http://localhost:8080/');
+var socket = io();
+
+class Input extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+    this.sendMessage = this.sendMessage.bind(this);
+  }
+  sendMessage(evt) {
+    evt.preventDefault();
+    socket.emit('chat message', { message: 'Hello' });
+    console.log('hi');
+  }
+
+  render() {
+    return (
+      <div>
+        <form>
+          <input placeholder="type here"></input>
+          <button
+            onClick={(evt) => {
+              this.sendMessage(evt);
+            }}
+          >
+            Send
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
 export default Input;
